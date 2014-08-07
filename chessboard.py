58ab2eb91f7374eb847febc19fe909916bd8d737
Blade Nelson <powderblock@gmail.com>
2014-08-07 04:09:12 -0400

    draw() added, imported chessboard.py into client.py
    Put the board draw into a function called draw, contained in
    chessboard.py, imported chessboard to client and added draw function
    call. Just testing.

--- chessboard.py
+++ chessboard.py
@@ -11,17 +11,18 @@ width, height = 8,8
 letters = {"A","B","C","D","E","F","G","H"}
 numbers = ['1','2','3','4','5','6','7','8',]
 
-#Nested for loop for board handling
-for x in range(0, width):
-    for y in range(0, height):
-        if y % 2 == x % 2:
-            print(Fore.WHITE+Back.WHITE+'-',end='')
+def draw():
+    #Nested for loop for board handling
+    for x in range(0, width):
+        for y in range(0, height):
+            if y % 2 == x % 2:
+                print(Fore.WHITE+Back.WHITE+'-',end='')
 
-        else:
-            print(Fore.YELLOW+Back.YELLOW+'-',end='')
+            else:
+                print(Fore.YELLOW+Back.YELLOW+'-',end='')
 
-    #Print numbers for the rows.
-    print(Fore.WHITE+Back.BLACK+' '+numbers[x])
+        #Print numbers for the rows.
+        print(Fore.WHITE+Back.BLACK+' '+numbers[x])
 
-#Print letters for the columns.
-print("ABCDEFGH")
+    #Print letters for the columns.
+    print("ABCDEFGH")
--- client.py
+++ client.py
@@ -1,8 +1,10 @@
 #This must be run in an interactive shell.
 import chess
+from chessboard import *
 
 game = chess.Game()
 chess.debug(True)
 player = game.join()
 gameinput = raw_input("Enter a game_id to join a game:\n")
 game = chess.Game(game_id=gameinput)
+draw()
